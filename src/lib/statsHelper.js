79989ba5f8a9f5aeff823890d8b885cc9608885b
user.email <julian.chan@mail.com>
2017-04-30 18:21:05 +1000

    changed MIN_STAT to 5

--- src/lib/statsHelper.js
+++ src/lib/statsHelper.js
@@ -1,6 +1,6 @@
 import { clamp, curry, merge } from 'ramda';
 
-const MIN_STAT = 0, MAX_STAT = 99;
+const MIN_STAT = 5, MAX_STAT = 99;
 
 const change = curry((amount, stat) =>
   merge(stat, { value: clamp(MIN_STAT, MAX_STAT, stat.value + amount) })
