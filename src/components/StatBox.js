299d9dda07620cda67c8413a31742f309249b0f6
user.email <julian.chan@mail.com>
2017-05-01 00:24:22 +1000

    StatBox now displays number of available stat points to spend (hardcoded to 10 for now)

--- src/components/StatBox.js
+++ src/components/StatBox.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { List, ListItem } from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
 import StatRow from './StatRow';
+
+const style = {
+  subheader: {
+    marginBottom: -24
+  }
+}
 
 const StatBox = ({ stats, handleIncrement, handleDecrement }) => (
   <List>
+    <Subheader style={style.subheader}>Available points: 10</Subheader>
     {stats.map(stat => (
       <ListItem key={stat.name}>
         <StatRow
